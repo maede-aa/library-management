56e03f71e1b2ee4204a88899d6d8b970763a26c6
maede <maede.aa99@gmail.com>
2025-06-01 18:36:26 +0330

    Completing the CustomQueue and CustomStack classes

--- src/datastructures/queues/CustomQueue.java
+++ src/datastructures/queues/CustomQueue.java
@@ -3,6 +3,8 @@ package datastructures.queues;
 import datastructures.interfaces.Queue;
 import datastructures.lists.CustomLinkedList;
 
+import java.util.NoSuchElementException;
+
 public class CustomQueue<T> implements Queue<T> {
     private CustomLinkedList<T> list;
 
@@ -10,6 +12,63 @@ public class CustomQueue<T> implements Queue<T> {
         list = new CustomLinkedList<>();
     }
 
-    // TODO: Override and fill the methods to complete the data structure
+    @Override
+    public boolean add(Object t) {
+        if(t == null)
+            throw new NullPointerException();
+
+        list.addLast((T) t);
+        return true;
+    }
+
+    @Override
+    public boolean offer(Object t) {
+        if(t == null)
+            return false;
+
+        list.addLast((T) t);
+        return false;
+    }
+
+    @Override
+    public T remove() {
+        if(list.isEmpty())
+            throw new NoSuchElementException();
+        return list.removeFirst();
+    }
+
+    @Override
+    public T poll() {
+        if(list.isEmpty())
+            return null;
+
+        return list.removeFirst();
+    }
+
+    @Override
+    public T element() {
+        if(list.isEmpty())
+            throw new NoSuchElementException();
+
+        return list.getFirst();
+    }
+
+    @Override
+    public T peek() {
+        if(list.isEmpty())
+            return null;
+
+        return list.getFirst();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
 
 }
--- src/datastructures/stacks/CustomStack.java
+++ src/datastructures/stacks/CustomStack.java
@@ -3,6 +3,8 @@ package datastructures.stacks;
 import datastructures.interfaces.Queue;
 import datastructures.lists.CustomLinkedList;
 
+import java.util.NoSuchElementException;
+
 public class CustomStack<T> implements Queue<T> {
     private CustomLinkedList<T> list;
 
@@ -10,6 +12,64 @@ public class CustomStack<T> implements Queue<T> {
         list = new CustomLinkedList<>();
     }
 
-    // TODO: Override and fill the methods to complete the data structure
+    @Override
+    public boolean add(Object t) {
+        if(t == null)
+            throw new NullPointerException();
+
+        list.addFirst((T) t);
+        return true;
+    }
+
+    @Override
+    public boolean offer(Object t) {
+        if(t == null)
+            return false;
+
+        list.addFirst((T) t);
+        return true;
+    }
+
+    @Override
+    public T remove() {
+        if(list.isEmpty())
+            throw new NoSuchElementException();
+
+        return list.removeFirst();
+    }
+
+    @Override
+    public T poll() {
+        if(list.isEmpty())
+            return null;
+
+        return list.removeFirst();
+    }
+
+    @Override
+    public T element() {
+        if(list.isEmpty())
+            throw new NoSuchElementException();
+
+        return list.getFirst();
+    }
+
+    @Override
+    public T peek() {
+        if(list.isEmpty())
+            return null;
+
+        return list.getFirst();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
 
 }
