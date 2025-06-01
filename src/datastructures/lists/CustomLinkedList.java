1c3d6478a5ea5e82de0f0d2702b5250b542b1079
maede <maede.aa99@gmail.com>
2025-06-01 16:56:14 +0330

    Completing the CustomLinkedList class.

--- src/datastructures/lists/CustomLinkedList.java
+++ src/datastructures/lists/CustomLinkedList.java
@@ -1,7 +1,168 @@
 package datastructures.lists;
 import datastructures.interfaces.LinkedList;
 
+import java.util.NoSuchElementException;
+
 public class CustomLinkedList<T> implements LinkedList<T>{
+    @Override
+    public void addFirst(T t) {
+        if(t == null)
+            throw new NullPointerException();
+
+        Node<T> node = new Node<>(t);
+        if(head == null)
+            head = tail = node;
+        else {
+            node.next = head;
+            head.prev = node;
+            head = node;
+        }
+
+        ++size;
+    }
+
+    @Override
+    public void addLast(T t) {
+        if(t == null)
+            throw new NullPointerException();
+
+        Node<T> node = new Node<>(t);
+        if(tail == null)
+            tail = head = node;
+        else {
+            tail.next = node;
+            node.prev = tail;
+            tail = node;
+        }
+
+        ++size;
+    }
+
+    @Override
+    public T removeFirst() {
+        if(head == null)
+            throw new NoSuchElementException();
+
+        T data = head.data;
+        head = head.next;
+        if(head == null)
+            tail = null;
+        else
+            head.prev = null;
+
+        --size;
+        return data;
+    }
+
+    @Override
+    public T removeLast() {
+        if(tail == null)
+            throw new NoSuchElementException();
+
+        T data = tail.data;
+        tail = tail.prev;
+        if(tail == null)
+            head = null;
+        else
+            tail.next = null;
+
+        --size;
+        return data;
+    }
+
+    @Override
+    public T getFirst() {
+        if(head == null)
+            throw new NoSuchElementException();
+
+        return head.data;
+    }
+
+    @Override
+    public T getLast() {
+        if(tail == null)
+            throw new NoSuchElementException();
+
+        return tail.data;
+    }
+
+    @Override
+    public void clear() {
+        head = tail = null;
+        size = 0;
+    }
+
+    @Override
+    public Object get(int index) {
+        if(index < 0 || index >= size)
+            throw new IndexOutOfBoundsException();
+
+        Node<T> current = head;
+        for(int i = 0 ; i < index ; ++i)
+            current = current.next;
+
+        return current.data;
+    }
+
+    @Override
+    public Object set(int index, Object element) {
+        if(index < 0 || index > size)
+            throw new IndexOutOfBoundsException();
+
+        Node<T> current = head;
+        for(int i = 0 ; i < index ; i++)
+            current = current.next;
+
+        T old = current.data;
+        current.data = (T) element;
+        return old;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean add(Object o) {
+        addLast((T) o);
+        return true;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        for(Node<T> current = head ; current != null ; current = current.next)
+            if(current.data.equals(o))
+                return true;
+
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        for(Node<T> current = head ; current != null ; current = current.next)
+            if(current.data.equals(o)) {
+                if(current.prev != null)
+                    current.prev.next = current.next;
+                else
+                    head = current.next;
+            if(current.next != null)
+                current.next.prev = current.prev;
+            else
+                tail = current.prev;
+
+            --size;
+            return true;
+            }
+
+        return false;
+    }
+
     private static class Node<T> {
         T data;
         Node<T> next;
@@ -14,7 +175,5 @@ public class CustomLinkedList<T> implements LinkedList<T>{
 
     private Node<T> head, tail;
     private int size = 0;
-
-    // TODO: Override and fill the methods to complete the data structure
 
 }
